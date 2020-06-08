14db85b96e41707ff4115e3de272c50aebfa8cc4
Tyler Damme <tyler.damme@gmail.com>
2020-06-08 13:45:49 -0500

    Bootstrap-Vue installed, simple navbar with interactive year created

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,39 @@
 <template>
- <p>hello</p>
+    <div>
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+        <b-navbar-brand>Big Ten Predictor</b-navbar-brand>
+
+        <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+            <b-nav-text>{{ timestamp }} Season</b-nav-text>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+    </div>
 </template>
 
 <script>
 export default {
-    
+
+    data() {
+        return {
+            timestamp: ""
+        };
+    },
+
+    created() {
+        this.getYear();
+    },
+
+    methods: {
+        getYear: function() {
+            const today = new Date();
+            let currentYear = today.getFullYear();
+            this.timestamp = currentYear;
+        }
+    }
 }
 </script>
 
